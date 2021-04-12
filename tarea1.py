9280ad33603121e626976d433525a2ee4eaa4228
unknownSystem <unknownsystem@pop-os.localdomain>
2021-04-12 16:18:59 -0400

    agregando tarea 1

---
+++ tarea1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data={
+    "Nombre": ["María", "Luis", "Carmen", "Antonio"],
+    "Edad": [18, 22,29, 21],
+    "Grado": ["Economía", "Medicina", "Arquitectura", "Economía"],
+    "Correo": ["maria@gmail.com", "luis@yahoo.es", "carmen@gmail.com", "antornio@gmail.com"]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
