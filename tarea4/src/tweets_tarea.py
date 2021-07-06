da0b7348cd69013dda18d7bc16ea53ac1f615dcd
Juan Pablo Nahuelpán <juanpablonahuelpan@gmail.com>
2021-07-06 01:28:24 -0400

    ed(4)

--- tarea4/src/tweets_tarea.py
+++ tarea4/src/tweets_tarea.py
@@ -1,6 +1,17 @@
-def ObtenerTweets(palabra="",times=10,lenguaje="en"):
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Integrantes: J.P. Nahuelpán
+# debe crear su propio keys.py con sus keys y tokens
+from keys import keys
+import tweepy
+from datetime import datetime
+from textblob import TextBlob
+import matplotlib.pyplot as plt
 
-    #Se define las listas que capturan la popularidad
+
+def ObtenerTweets(palabra="", times=10, lenguaje="en"):
+
+    # Se define las listas que capturan la popularidad
 
     popularidad_list = []
 
@@ -8,13 +19,14 @@ def ObtenerTweets(palabra="",times=10,lenguaje="en"):
 
     numero = 1
 
-    for tweet in tweepy.Cursor(api.search, palabra, lang="en").items(numero_de_Tweets):
+    for tweet in tweepy.Cursor(api.search, palabra,
+                               lang="en").items(numero_de_Tweets):
 
         try:
 
-            #Se toma el texto, se hace el analisis de sentimiento
+            # Se toma el texto, se hace el analisis de sentimiento
 
-            #y se agrega el resultado a las listas
+            # y se agrega el resultado a las listas
             print(tweet.created_at)
             print(tweet.text)
 
@@ -23,7 +35,7 @@ def ObtenerTweets(palabra="",times=10,lenguaje="en"):
             analisis = analisis.sentiment
 
             popularidad = analisis.polarity
-            print("Polaridad sentimiento:",popularidad)
+            print("Polaridad sentimiento:", popularidad)
 
             popularidad_list.append(popularidad)
 
@@ -31,31 +43,20 @@ def ObtenerTweets(palabra="",times=10,lenguaje="en"):
 
             numero = numero + 1
 
-
-
         except tweepy.TweepError as e:
-
             print(e.reason)
 
-
-
         except StopIteration:
-
             break
 
-    return (numeros_list,popularidad_list,numero)
+    return (numeros_list, popularidad_list, numero)
 
 
-
-
-
-def GraficarDatos(numeros_list,popularidad_list,numero):
+def GraficarDatos(numeros_list, popularidad_list, numero):
 
     axes = plt.gca()
 
     axes.set_ylim([-1, 2])
-
-
 
     plt.scatter(numeros_list, popularidad_list)
 
@@ -63,83 +64,51 @@ def GraficarDatos(numeros_list,popularidad_list,numero):
 
     popularidadPromedio = "{0:.0f}%".format(popularidadPromedio * 100)
 
-    time  = datetime.now().strftime("A : %H:%M\n El: %m-%d-%y")
+    time = datetime.now().strftime("A : %H:%M\n El: %m-%d-%y")
 
-    plt.text(0, 1.25, 
-
-             "Sentimiento promedio:  " + str(popularidadPromedio) + "\n" + time, 
-
-             fontsize=12, 
-
-             bbox = dict(facecolor='none', 
-
-                         edgecolor='black', 
-
-                         boxstyle='square, pad = 1'))
-
-
+    plt.text(0, 1.25,
+             "Sentimiento promedio:  "
+             + str(popularidadPromedio)
+             + "\n" + time,
+             fontsize=12,
+             bbox=dict(facecolor='none',
+                       edgecolor='black',
+                       boxstyle='square, pad = 1'))
 
     plt.title("Sentimientos sobre " + palabra + " en twitter")
-
     plt.xlabel("Numero de tweets")
-
     plt.ylabel("Sentimiento")
-
     plt.show()
 
 
-#Se importa la librería tweepy
+# Se define las variables para el acceso al API de twitter
 
-import tweepy
+# keys = [consumer_key, consumer_key_secret, access_token, access_token_secret]
 
-#Se importa sleep, datetime, TextBlob y matplotlib
-
-from datetime import datetime
-
-from textblob import TextBlob 
-
-import matplotlib.pyplot as plt 
-
-#Se define las variables para el acceso al API de twitter
-
-consumer_key = "MmzMhiZfPtlOM1dzjdOfMP8VU"
-consumer_key_secret = "6bqcYAIC9qsBKf7KWK0H3oI5k7IXrveNyqM1T6nTsjiaosQ8yw"
-access_token = "1396927562220281861-bDH6WUiFuFFTNxZ0lh8O6tC1Ma8RmM"
-access_token_secret = "Ox3Ih01iP7ETUANryGecRo4HhyP4PyxlAIwGf8UUIRZzH"
-
-#Se autentica en twitter
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
-
-auth.set_access_token(access_token, access_token_secret)
-
+# Se autentica en twitter
+auth = tweepy.OAuthHandler(keys[0], keys[1])
+auth.set_access_token(keys[2], keys[3])
 api = tweepy.API(auth)
 
-
-
-#se verifica que el usuario conectado en twitter es de uno
+# se verifica que el usuario conectado en twitter es de uno
 
 print(api.me().name)
 
-#Se pregunta por la palabra a preguntar
+# Se pregunta por la palabra a preguntar
 
 palabra = input("Buscar: ")
-
-
-
-#Se define la cantida de tweets a capturar
-
+# Se define la cantida de tweets a capturar
 numero_de_Tweets = int(input("Número de tweets a capturar: "))
-
-
 lenguaje = input("Idioma [es/en]:")
 
-if lenguaje=="es":
-   palabra = TextBlob(palabra)
-   palabra = palabra.translate(to='en')
-   palabra = str(palabra) 
+if lenguaje == "es":
+    palabra = TextBlob(palabra)
+    palabra = palabra.translate(to="en")
+    palabra = str(palabra)
 
 
-numeros_list,popularidad_list,numero = ObtenerTweets(palabra,numero_de_Tweets,lenguaje)
+numeros_list, popularidad_list, numero = ObtenerTweets(palabra,
+                                                       numero_de_Tweets,
+                                                       lenguaje)
 
-GraficarDatos(numeros_list,popularidad_list,numero)
+GraficarDatos(numeros_list, popularidad_list, numero)
---
+++ tarea5/src/main.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# integrantes: J.P. Nahuelpán, Pelegrin Huichalaf
+import os
+import tweepy
+from textblob import TextBlob
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use('tkAgg')# para linux.
+import matplotlib.pyplot as plt
+import re
+from wordcloud import WordCloud
+import json
+from keys import keys # debe crear su propio keys.py con sus keys y tokens
+# keys = [consumer_key, consumer_key_secret, access_token, access_token_secret]
+
+ruta_src = os.path.dirname(os.path.realpath(__file__)) 
+ruta_input = ruta_src.replace("src", "input/")
+ruta_output = ruta_src.replace("src", "output/")
+
+
+auth = tweepy.OAuthHandler(keys[0], keys[1])
+auth.set_access_token(keys[2], keys[3])
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# Definiendo la palabra de búsqueda y el número de tweets
+query = str(input("Ingrese palabra clave: "))
+query = TextBlob(query)
+max_tweets = int(input("Numero de tweets: "))
+searched_tweets = [status for status in tweepy.Cursor(
+                                                      api.search,
+                                                      q=query).items(
+                                                      max_tweets)]
+
+# Informe de Analisis de Sentimiento
+# Encontrando polaridad (pos neg and neutral)
+obj = 0
+sub = 0
+for tweet in searched_tweets:
+    analysis = TextBlob(tweet.text)
+    if analysis.sentiment[1]>0.5:
+       sub = sub + 1
+    elif analysis.sentiment[1]<0.5:
+       obj = obj + 1
+    #else:
+       #neu = neu + 1
+print("Total subjetivo = ", sub)
+print("Total objetivo = ", obj)
+
+#Graficando Circular Sentimiento
+labels = 'obj', 'sub'
+sizes = [obj, sub]
+colors = ['blue', 'red']
+explode = (0.1, 0)  # explode 1st slice
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
+
+# Dataframe de Tweets
+# Limpiando tweets y convirtiendo a Dataframe
+my_list_of_dicts = []
+for each_json_tweet in searched_tweets:
+    my_list_of_dicts.append(each_json_tweet._json)
+with open(ruta_input+'tweet_json_Data.txt', 'w') as file:
+    file.write(json.dumps(my_list_of_dicts, indent=4))
+
+my_demo_list = []
+with open(ruta_input+'tweet_json_Data.txt', encoding='utf-8') as json_file:
+    all_data = json.load(json_file)
+    for each_dictionary in all_data:
+        tweet_id = each_dictionary['id']
+        text = each_dictionary['text']
+        favorite_count = each_dictionary['favorite_count']
+        retweet_count = each_dictionary['retweet_count']
+        created_at = each_dictionary['created_at']
+        my_demo_list.append({'tweet_id': str(tweet_id),
+                             'text': str(text),
+                             'favorite_count': int(favorite_count),
+                             'retweet_count': int(retweet_count),
+                             'created_at': created_at})
+        tweet_dataset = pd.DataFrame(my_demo_list,
+                                     columns=['tweet_id', 'text',
+                                              'favorite_count',
+                                              'retweet_count',
+                                              'created_at'])
+
+# Guardando el dataset de tweets en un archivo CSV
+tweet_dataset.to_csv(ruta_output+'tweet_data.csv')
+
+# tweet_dataset.shape
+
+# tweet_dataset.head()
+
+# Limpiando datos
+
+
+def remove_pattern(input_txt, pattern):
+    r = re.findall(pattern, input_txt)
+    for i in r:
+        input_txt = re.sub(i, '', input_txt)
+    return input_txt
+
+
+tweet_dataset['text'] = np.vectorize(
+                                     remove_pattern)(
+                                     tweet_dataset['text'],
+                                     "@[\w]*")
+
+# tweet_dataset.head()
+
+# tweet_dataset['text'].head()
+
+# Limpiando Tweets
+corpus = []
+for i in range(0, max_tweets):
+    tweet = re.sub('[^a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]', ' ', tweet_dataset['text'][i])
+    tweet = tweet.lower()
+    tweet = re.sub('rt', '', tweet)
+    tweet = re.sub('http', '', tweet)
+    tweet = re.sub('https', '', tweet)
+    tweet = tweet.split()
+# ps = PorterStemmer()
+# tweet = [ps.stem(word) for word in tweet if not
+# word in set(stopwords.words('english'))]
+    tweet = ' '.join(tweet)
+    corpus.append(tweet)
+
+# Word Cloud o Nube de Palabras
+all_words = ' '.join([text for text in corpus])
+wc = WordCloud().generate(all_words)
+plt.figure(figsize=(10, 7))
+plt.imshow(wc, interpolation="bilinear")
+plt.axis('off')
+plt.show()
